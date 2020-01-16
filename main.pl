fb8e019ba805f318e2634b43ab81cef19766c875
unknown <ra99829@uem.br>
2020-01-16 00:02:02 -0300

    quase tudo pronto

--- main.pl
+++ main.pl
@@ -1,3 +1,4 @@
+:-use_module(library(tty)).
 :-include('questionario.pl').
 
 % Predicado para o calculo dos niveis de transtorno
@@ -6,35 +7,69 @@
 % Os acumuladores S0, S1, S2, S3, S4, S5 e S6 são para calculo do nível de transtorno de Estresse
 questionario :-
 	questao_1(0,D0),
+	tty_clear/0,
 	questao_2(0,A0),
+	tty_clear/0,
 	questao_3(0,S0),
+	tty_clear/0,
 	questao_4(A0,A1),
+	tty_clear/0,
 	questao_5(D0,D1),
+	tty_clear/0,
 	questao_6(S0,S1),
+	tty_clear/0,
 	questao_7(A1,A2),
+	tty_clear/0,
 	questao_8(S1,S2),
+	tty_clear/0,
 	questao_9(A2,A3),
+	tty_clear/0,
 	questao_10(D1,D2),
+	tty_clear/0,
 	questao_11(S2,S3),
+	tty_clear/0,
 	questao_12(S3,S4),
+	tty_clear/0,
 	questao_13(D2,D3),
+	tty_clear/0,
 	questao_14(S4,S5),
+	tty_clear/0,
 	questao_15(A3,A4),
+	tty_clear/0,
 	questao_16(D3,D4),
+	tty_clear/0,
 	questao_17(D4,D5),
+	tty_clear/0,
 	questao_18(S5,S6),
+	tty_clear/0,
 	questao_19(A4,A5),
+	tty_clear/0,
 	questao_20(A5,A6),
+	tty_clear/0,
 	questao_21(D5,D6),
+	tty_clear/0,
 	X = D6,
 	Y = A6,
 	Z = S6,
-	writeln('Transtornos'),
-	writeln(''),
+	write_ln('Transtornos'),
+	write_ln(''),
 	write('Depressão: '),
-	grau(doenca(depressao),X),
+	grau(doenca(depressao),X, Out_Depression),
 	write('Ansiedade: '),
-	grau(doenca(ansiedade),Y),
+	grau(doenca(ansiedade),Y, Out_Anxiety),
 	write('Estresse: '),
-	grau(doenca(stress),Z),
-	writeln('').
+	grau(doenca(stress),Z, Out_Stress),
+	write_ln('').
+
+main :-
+	:-write_ln('Segundo a OMS, “a saúde é um estado de completo bem-estar físico, mental e social e não apenas a ausência de doença ou enfermidade”. Mais especificamente, a saúde mental é um termo usado para descrever o nível de qualidade de vida cognitiva ou emocional. Dentro deste contexto, cada vez mais é necessário que conheçamos nossas forças e dificuldades, e para tal qualquer ação que aperfeiçoe o autoconhecimento pode auxiliar.')
+	write_ln('Tendo isso em mente, o objetivo do seguinte protótipo baseado no DASS-21 é calcular a escala de estresse, ansiedade e depressão, avaliando os resultados e mostrando a severidade dos sintomas dos disturbios mentais associados as doenças.').
+	write_ln('O DASS-21 usa um sistema de cálculo baseado em 21 perguntas, sendo 7 para cada doença, onde a pontuação máxima é 21 para cada doença.').
+	write_ln('Os níveis de doença são : Não há disturbio, Nivel médio de disturbio e Alto nível de Disturbio').
+	write_ln('É importante notar que a resposta do usuario deve ser em um inteiro entre 0 a 3, e após cada resposta deve ser colocado o ponto final.').
+	write_ln('As respostas são :').
+	write_ln('0 - Nunca').
+	write_ln('1 - Agumas vezes').
+	write_ln('2 - Frequentemente').
+	write_ln('3 - Quase sempre').
+	questionario.
--- questionario.pl
+++ questionario.pl
@@ -1,3 +1,5 @@
+:-use_module(library(plunit))
+
 % Questões para calcular o grau das seguintes doenças : Depressao, Ansiedade e Stress
 % O cálculo do grau de doença é baseado no DASS-21
 % Algumas questões foram retiradas do próprio DASS-21, outras foram de testes disponiveis no https://www.psycom.net/, link disponibilizado pelo professor
@@ -21,149 +23,283 @@
 
 questao_1(Depression,Result) :-
 	write_ln('Eu sinto pouco interesse ou prazer em fazer as coisas'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Depression + X.
 
 questao_2(Anxiety,Result) :-
 	write_ln('Eu percebi que estava com a boca seca'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Anxiety + X.
 
 questao_3(Stressed,Result) :-
 	write_ln('Você já experimentou algum dos seguintes sintomas: dores de cabeça, dor no peito, tensão muscular, náusea ou alterações no desejo sexual?'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Stressed + X.
 
 questao_4(Anxiety,Result) :-
 	write_ln('Eu tive dificuldade para respirar '),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Anxiety + X.
 
 questao_5(Depression,Result) :-
 	write_ln('Eu me senti pra baixo, deprimido ou sem esperança'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Depression + X.
 
 questao_6(Stressed,Result) :-
 	write_ln('Em geral, tive reações exageradas às situações'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Stressed + X.
 
 questao_7(Anxiety,Result) :-
 	write_ln('Tive tremores (por exemplo, nas mãos)'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Anxiety + X.
 
 questao_8(Stressed,Result) :-
 	write_ln('Você sente fadiga e / ou teve dificuldade em adormecer ou ficar dormindo'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Stressed + X.
 
 questao_9(Anxiety,Result) :-
 	write_ln('Eu fiquei preocupado(a) com situações em que poderia entrar em pânico e fazer papel de bobo(a)'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Anxiety + X.
 
 questao_10(Depression,Result) :-
 	write_ln('Eu senti que não tinha expectativas positivas a respeito de nada'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Depression + X.
 
 questao_11(Stressed,Result) :-
 	write_ln('Você tem dificuldade em se concentrar nas tarefas ou se manter motivado?'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Stressed + X.
 
 questao_12(Stressed,Result) :-
 	write_ln('Você sente irritabilidade, tristeza ou raiva?'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Stressed + X.
 
 questao_13(Depression,Result) :-
 	write_ln('Eu tive problemas para adormecer ou dormi demais'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Depression + X.
 
 questao_14(Stressed,Result) :-
 	write_ln('Eu não tive paciência com coisas que interromperam o que estava fazendo'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Stressed + X.
 
 questao_15(Anxiety,Result) :-
 	write_ln('Eu senti que estava prestes a entrar em pânico'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Anxiety + X.
 
 questao_16(Depression,Result) :-
 	write_ln('Tive pouco apetite ou comi demais'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Depression + X.
 
 questao_17(Depression,Result) :-
 	write_ln('Eu senti que não tinha muito valor como pessoa'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Depression + X.
 
 questao_18(Stressed,Result) :-
 	write_ln('Você sente dificuldades para regular a quantidade de cafeína, álcool ou tabaco que usa?'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Stressed + X.
 
 questao_19(Anxiety,Result) :-
 	write_ln('Eu percebi as batidas do meu coração mais aceleradas sem ter feito esforço físico'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Anxiety + X.
 
 questao_20(Anxiety,Result) :-
 	write_ln('Eu evito lugares ou situações sociais por ter medo de entrar em pânico'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Anxiety + X.
 
 questao_21(Depression,Result) :-
 	write_ln('Eu tive pensamentos que seria melhor estar morto, ou pensamentos em me machucar'),
+	write_ln('0 - Nunca'),
+	write_ln('1 - Agumas vezes'),
+	write_ln('2 - Frequentemente'),
+	write_ln('3 - Quase sempre'),
 	read(user_input,X),
 	Result is Depression + X.
 
 % doencas a serem calculadas e sua forma de calcular
-doenca(deprecao).
+doenca(depressao).
 doenca(ansiedade).
 doenca(stress).
 
-grau(doenca(depressao),X) :-
+% Y is the variable that calculates witch level of the disease the user have
+% if Y = 1, there is no signs of the disease, its Normal
+% if Y = 2, the user have a medium level of the disease
+% if Y = 3, the user have a high level of the disease
+% Y is also used to make the tests
+
+%% grau(doenca(alguma_doenca)+x,?y) is semidet
+% verdadeira se Y indica o nivel correto do grau de doenca, de acordo com o valor de X
+
+:-begin_tests(grau).
+test(grau1) :- grau(doenca(depressao), 0, 1).
+test(grau2) :- grau(doenca(depressao), 3, 1).
+test(grau3, [fail]) :- grau(doenca(depressao), 4, 1).
+test(grau4) :- grau(doenca(depressao), 4, 2).
+test(grau5) :- grau(doenca(depressao), 10, 2).
+test(grau6, [fail]) :- grau(doenca(depressao), 11, 2).
+test(grau7) :- grau(doenca(depressao), 11, 3).
+test(grau8) :- grau(doenca(depressao), 21, 3).
+test(grau9, [fail]) :- grau(doenca(depressao), 22, 3).
+test(grau10) :- grau(doenca(ansiedade), 0, 1).
+test(grau11) :- grau(doenca(ansiedade), 3, 1).
+test(grau12, [fail]) :- grau(doenca(ansiedade), 4, 1).
+test(grau13) :- grau(doenca(ansiedade), 4, 2).
+test(grau14) :- grau(doenca(ansiedade), 7, 2).
+test(grau15, [fail]) :- grau(doenca(ansiedade), 8, 2).
+test(grau16) :- grau(doenca(ansiedade), 8, 3).
+test(grau17) :- grau(doenca(ansiedade), 21, 3).
+test(grau18, [fail]) :- grau(doenca(ansiedade), 22, 3).
+test(grau19) :- grau(doenca(stress), 0, 1).
+test(grau20) :- grau(doenca(stress), 7, 1).
+test(grau21, [fail]) :- grau(doenca(stress), 8, 1).
+test(grau22) :- grau(doenca(stress), 8, 2).
+test(grau23) :- grau(doenca(stress), 12, 2).
+test(grau24, [fail]) :- grau(doenca(stress), 13, 2).
+test(grau25) :- grau(doenca(stress), 13, 3).
+test(grau26) :- grau(doenca(stress), 21, 3).
+test(grau27, [fail]) :- grau(doenca(stress), 22, 3).
+
+grau(doenca(depressao),X, Y) :-
 	X < 4,
+	Y is 1,
 	write_ln('Não há transtorno').
 
-grau(doenca(depressao),X) :-
+grau(doenca(depressao),X, Y) :-
 	X >= 4,
 	X < 11,
+	Y is 2,
 	write_ln('Médio grau/Nível de transtorno').
 
-grau(doenca(depressao),X) :-
+grau(doenca(depressao),X, Y) :-
 	X >= 11,
+	X < 22,
+	Y is 3,
 	write_ln('Alto grau/Nível  de transtorno').
 
-grau(doenca(ansiedade),X) :-
+grau(doenca(ansiedade),X, Y) :-
 	X < 4,
+	Y is 1,
 	write_ln('Não há transtorno').
 
-grau(doenca(ansiedade),X) :-
+grau(doenca(ansiedade),X, Y) :-
 	X >= 4,
 	X < 8,
+	Y is 2,
 	write_ln('Médio grau/Nível  de transtorno').
 
-grau(doenca(ansiedade),X) :-
+grau(doenca(ansiedade),X, Y) :-
 	X >= 8,
+	X < 22,
+	Y is 3,
 	write_ln('Alto grau/Nível  de transtorno').
 
-grau(doenca(stress),X) :-
-	X < 7,
+grau(doenca(stress),X, Y) :-
+	X < 8,
+	Y is 1,
 	write_ln('Não há transtorno').
 
-grau(doenca(stress),X) :-
+grau(doenca(stress),X, Y) :-
 	X >= 8,
 	X < 13,
+	Y is 2,
 	write_ln('Médio grau/Nível  de transtorno').
 
-grau(doenca(stress),X) :-
+grau(doenca(stress),X, Y) :-
 	X >= 13,
+	X < 22,
+	Y is 3,
 	write_ln('Alto grau/Nível  de transtorno').
